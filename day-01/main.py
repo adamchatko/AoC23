f327f6b479e94eec4e4bb3f178bb7ba35ff5baca
Adam Julian <adam@julian.si>
2023-12-15 21:09:36 +0100

    Solve Part 2

--- day-01/main.py
+++ day-01/main.py
@@ -1,24 +1,43 @@
-from icecream import ic
-
 with open("input.txt", "r") as file:
 	lines = [line.rstrip() for line in file]
 
 cal_values_list = []
+numbers_as_strings = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 for line in lines:
-	line_list = [*line]
-	new_line_list = []
-	for i in range(len(line_list)):
+	line_as_list = [*line]
+	numbers_dict = {}
+	# for item in line_as_list:
+	# 	try:
+	# 		item_as_int = int(item)
+	# 		numbers_dict[item] = line_as_list.index(item)
+	# 	except ValueError:
+	# 		continue
+
+	for index, value in enumerate(line):
 		try:
-			new_line_list.append(int(line_list[i]))
+			item_as_int = int(value)
+			number = numbers_as_strings[item_as_int]
+			numbers_dict[f"{number}-{index}"] = index
 		except ValueError:
-			continue
+			for number in numbers_as_strings:
+				substring = line[index:index + len(number)]
+				if substring == number:
+					numbers_dict[f"{number}-{index}"] = index
+					continue
 
-	if len(new_line_list) == 1:
-		cal_value = int(f"{new_line_list[0]}{new_line_list[0]}")
-	else:
-		cal_value = int(f"{new_line_list[0]}{new_line_list[-1]}")
+	first_digit_key = min(numbers_dict, key=numbers_dict.get)
+	last_digit_key = max(numbers_dict, key=numbers_dict.get)
+	first_digit = 0
+	last_digit = 0
 
+	for number in numbers_as_strings:
+		if first_digit_key.startswith(number):
+			first_digit = numbers_as_strings.index(number)
+		if last_digit_key.startswith(number):
+			last_digit = numbers_as_strings.index(number)
+
+	cal_value = int(f"{first_digit}{last_digit}")
 	cal_values_list.append(cal_value)
 
 cal_values_sum = sum(cal_values_list)
